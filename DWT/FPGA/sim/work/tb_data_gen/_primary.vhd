b3ee7653f86dc0df8068bdb1a6a1723086adac3a
yunsong1 <1591494551@qq.com>
2024-05-26 16:10:03 +0800

    fix

--- DWT/FPGA/sim/work/tb_data_gen/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity tb_data_gen is
-    generic(
-        PERIOD          : integer := 10
-    );
-    attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of PERIOD : constant is 1;
-end tb_data_gen;
